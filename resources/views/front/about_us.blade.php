5aa560207a09bec600331d7386dbf0dd4bddad19
ngthanh <thanh.nn1106@gmail.com>
2017-12-12 02:37:52 +0700

    fix bug

--- resources/views/front/about_us.blade.php
+++ resources/views/front/about_us.blade.php
@@ -1,11 +1,9 @@
 @extends('front.layout')
 
 @section('content')
-<!-- wrapper -->
 <h1 class="logo">GIỚI THIỆU - LỊCH SỬ THÀNH LẬP - NGƯỜI ĐIỀU HÀNH</h1>
 <h5>Hệ thống Trường Thẩm Mỹ BaLê là trường chuyên dạy các ngành thẩm mỹ đầu tiên tại Việt Nam. Hệ thống Trường Thẩm Mỹ BaLê có 3 chi nhánh tại Hoa Kỳ ( Denver Co, Arlington tx, Houston tx ), Trường quy tụ nhiều giáo viên Việt Nam và nước ngoài nổi tiếng trong ngành thẩm mỹ. Cô Christina Trần là thành viên ban giám khảo chấm thi ngành thẩm mỹ Châu Á và Quốc Tế. Cô Vân Như - Giám đốc chi nhánh Trường Thẩm Mỹ  BaLê tại Việt Nam, là người Việt Nam đầu tiên liên tiếp 3 năm liền đoạt cúp và huy chương đồng quốc tế được tổ chức tại Hoa Kỳ về nghệ thuật trang điểm và hóa trang. Đặc biệt Trường Thẩm Mỹ  BaLê là trường đầu tiên tại Việt Nam được tổng cục dạy nghề cấp giấy phép đào tạo nghề người mẫu.</h5>
 <br>
-<div class="loader_div" id="loader">Laster innhold, vennligst vent!</div>
 <div id="pagination_content">
     <div style="overflow:auto; position:relative;">
         <div class="article_box2"> <a href="#"><img src="images/bale/conhung.jpg" alt="" width="106" height="126" style="float:left; margin-right: 10px"></a>
@@ -42,7 +40,4 @@
     </div>
     <div style="height:15px;">&nbsp;</div>
 </div>
-<div style="height: 5px; clear:both"></div>
-</div>
-<div style="clear:both"></div>
 @endsection
